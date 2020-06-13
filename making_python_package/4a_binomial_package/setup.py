3ed28dc85b74ddeb1c84f525d6c4a83252e0020e
marslanali <arslanali800@hotmail.com>
2020-06-13 19:52:51 +0500

    binomial_package

--- making_python_package/4a_binomial_package/setup.py
+++ making_python_package/4a_binomial_package/setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
 
-setup(name='distributions',
-      version='0.1',
-      description='Gaussian distributions',
-      packages=['distributions'],
+setup(name='binomial_package',
+      version='1.1',
+      description='Gaussian and binomial distributions',
+      packages=['binomial_package'],
+      author='Arslan Ali',
+      keywords = ['binomial', 'gaussian', 'distribution', 'udacity'],
       zip_safe=False)
--- making_python_package/4a_binomial_package/test.py
+++ making_python_package/4a_binomial_package/test.py
@@ -6,13 +6,13 @@
 
 import unittest
 
-from distributions import Gaussian
-from distributions import Binomial
+from binomial_package import Gaussian
+from binomial_package import Binomial
 
 class TestGaussianClass(unittest.TestCase):
     def setUp(self):
         self.gaussian = Gaussian(25, 2)
-        self.gaussian.read_data_file('numbers.txt')
+        self.gaussian.read_data_file('/home/arslan/github-repositories/DSND_Term2/making_python_package/4a_binomial_package/binomial_package/numbers.txt')
 
     def test_initialization(self): 
         self.assertEqual(self.gaussian.mean, 25, 'incorrect mean')
@@ -49,7 +49,7 @@ class TestGaussianClass(unittest.TestCase):
 class TestBinomialClass(unittest.TestCase):
     def setUp(self):
         self.binomial = Binomial(0.4, 20)
-        self.binomial.read_data_file('numbers_binomial.txt')
+        self.binomial.read_data_file('/home/arslan/github-repositories/DSND_Term2/making_python_package/4a_binomial_package/binomial_package/numbers_binomial.txt')
 
     def test_initialization(self):
         self.assertEqual(self.binomial.p, 0.4, 'p value incorrect')
